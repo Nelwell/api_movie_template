7e9c8e4c6e19a290cbba5efa529236514e50c49a
Nelwell <nick.elwell206@gmail.com>
2019-11-09 04:39:08 -0600

    Finished from slides.

--- app/src/main/java/com/clara/apimovies/view/AddMovieFragment.java
+++ app/src/main/java/com/clara/apimovies/view/AddMovieFragment.java
@@ -2,6 +2,7 @@ package com.clara.apimovies.view;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.clara.apimovies.model.Movie;
@@ -43,7 +45,8 @@ public class AddMovieFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // TODO initialize mMovieViewModel
+        // initialize mMovieViewModel
+        mMovieViewModel = ViewModelProviders.of(getActivity()).get(MovieViewModel.class);
 
     }
 
@@ -67,7 +70,20 @@ public class AddMovieFragment extends Fragment {
                 float rating = movieRating.getRating();   //how many stars selected
                 Movie movie = new Movie(name, rating);
 
-                // TODO use mMovieViewModel to insert new movie
+                // use mMovieViewModel to insert new movie
+                mMovieViewModel.insert(movie).observe(getActivity(), new Observer<String>() {
+                    @Override
+                    public void onChanged(String s) {
+                        Log.d(TAG, "s" + s);
+                        if (s.equals("success")) {
+                            Toast.makeText(getActivity(), "Movie added!", Toast.LENGTH_SHORT).show();
+                        } else if (s.contains("duplicate key")) {
+                            Toast.makeText(getActivity(), "You already added that movie!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getActivity(), "Error adding movie", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
                 newMovieListener.onMovieAdded(movie);  // notifies Activity so fragments can be swapped
 
